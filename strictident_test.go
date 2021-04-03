f55fb34fd38e2d44d44305a4d8087cbcb98fb3d9
Cheyin L <cheyinl@gmail.com>
2021-04-03 11:18:52 +0800

    refactor: extract identifier path check function

--- strictident_test.go
+++ strictident_test.go
@@ -32,28 +32,6 @@ func TestStrictIdentifierNormalize(t *testing.T) {
 	}
 }
 
-func verifyIdentifierPathResult(t *testing.T, inputV, expV string, expF []string, resultV string, resultF []string) (successVerify bool) {
-	successVerify = true
-	if expV != resultV {
-		t.Errorf("unexpect identifier path result [input=%s]: [%s] vs. [%s]", inputV, expV, resultV)
-		successVerify = false
-	}
-	if len(expF) != len(resultF) {
-		t.Errorf("unexpect identifier fragements size [input=%s]: %d vs. %d", inputV, len(expF), len(resultF))
-		successVerify = false
-	}
-	for idx, vF := range expF {
-		if idx == len(resultF) {
-			break
-		}
-		if vF != resultF[idx] {
-			t.Errorf("unexpect identifier fragement [input=%s, index=%d]: [%s] vs. [%s]", inputV, idx, vF, resultF[idx])
-			successVerify = false
-		}
-	}
-	return
-}
-
 func checkStrictIdentifierPathNormalize(t *testing.T,
 	inputV string, separatorCh rune, maxIdentifierPathLength int, identTransFunc identnormalize.IdentifierTransformFunc,
 	expV string, expF []string) (successVerify bool) {
